9fa8a889db98ca8f43e974405bc702c9c77d1bf4
Alexander Philippopoulos <161390808+aphilippopoulos248@users.noreply.github.com>
2025-05-28 12:15:44 -0400

    Updated menu scene

--- jurassic-world-app/src/data/dinos.js
+++ jurassic-world-app/src/data/dinos.js
@@ -4,6 +4,16 @@ const Dinos_Data = [
         name:"Tyrannosaurus Rex",
         link:"/lab",
     },
+    {
+        no:"02",
+        name:"Velociraptor",
+        link:"/lab",
+    },
+    {
+        no:"03",
+        name:"Triceratops",
+        link:"/lab",
+    },
 ]
  
 export default Dinos_Data;
--- jurassic-world-app/src/scenes/MenuScene/MenuScene.jsx
+++ jurassic-world-app/src/scenes/MenuScene/MenuScene.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
 import Dinos_Data from '../../data/dinos';
+import './MenuScene.scss';
 
 const MenuScene = () => {
+  useEffect(() => {
     const scene = new THREE.Scene();
-    // Sizes
+
     const sizes = {
       width: window.innerWidth,
       height: window.innerHeight,
     };
+
     const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100);
     camera.position.z = 20;
     scene.add(camera);
-    // Play music
+
+    // Audio setup
     const listener = new THREE.AudioListener();
     camera.add(listener);
 
-    const audioLoader = new THREE.AudioLoader();
     const bgMusic = new THREE.Audio(listener);
+    const audioLoader = new THREE.AudioLoader();
 
     audioLoader.load('../music/jurassic-world-bgm2.mp3', function (buffer) {
         bgMusic.setBuffer(buffer);
@@ -25,23 +29,31 @@ const MenuScene = () => {
         bgMusic.setVolume(1);
         bgMusic.play();
     });
-    
-  return (
-    <div className="dinos-container">
-        {Dinos_Data.map((dino, index)=>{
-            return <div className='dinos-format'>
-                <h3>{dino.no}</h3>
-                <h2>{dino.name}</h2>
-                <div className='services-readmore'>
-                    <a href={dino.link}>
-                        <p>Click Here</p>
-                        {/* <img src={arrow_icon} alt="" /> */}
-                    </a>
-                </div>
-            </div>
-        })}
-    </div>
-  )
-}
 
-export default MenuScene
+    return () => {
+      if (bgMusic && bgMusic.isPlaying) {
+        bgMusic.stop();
+      }
+    };
+  }, []);
+
+  return (
+    <div className="dinos">
+      <div className="dinos-container">
+        {Dinos_Data.map((dino, index) => (
+          <div className='dinos-format' key={index}>
+            <h3>{dino.no}</h3>
+            <h2>{dino.name}</h2>
+            <div className='services-readmore'>
+              <a href={dino.link}>
+                <p>Click Here</p>
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MenuScene;
