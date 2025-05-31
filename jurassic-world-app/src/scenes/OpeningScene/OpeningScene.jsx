f15f6220281b722943bc578989888de2a90f840a
Alexander Philippopoulos <161390808+aphilippopoulos248@users.noreply.github.com>
2025-05-31 11:59:19 -0400

    Updated opening scene

--- jurassic-world-app/src/scenes/OpeningScene/OpeningScene.jsx
+++ jurassic-world-app/src/scenes/OpeningScene/OpeningScene.jsx
@@ -15,8 +15,8 @@ const OpeningScene = () => {
       // Timeline for animations
       const showClickHere = () => {
         const tl = gsap.timeline({ defaults: { duration: 1 } });
-        tl.fromTo('.enter', { scaleX: 0 }, { scaleX: 1, transformOrigin: 'center', duration: 1, delay: 10, ease: 'power1.out' });
-        tl.fromTo('.enter p', { opacity: 0 }, { opacity: 0.8, duration: 5, delay: 0.5 });
+        tl.fromTo('.enter', { scaleX: 0 }, { scaleX: 1, transformOrigin: 'center', duration: .5, delay: 10, ease: 'power1.out' });
+        tl.fromTo('.enter p', { opacity: 0 }, { opacity: 0.8, duration: 1, delay: 0.5 });
 
         window.removeEventListener('click', showClickHere);
       }
